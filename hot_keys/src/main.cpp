9613a926ee1fe731b398d69f2f1b3714866af028
b1tfly <thisisandrew0110@gmail.com>
2023-07-10 22:17:30 +0300

    added test case

--- hot_keys/src/main.cpp
+++ hot_keys/src/main.cpp
@@ -1,5 +1,24 @@
+#include "../parser/parser.hpp"
+
+// TODO: create models single button && pair of button ?
+
 int main()
 {  
+    Parser parser;
+    parser.m_parse_all_data();
+    parser.m_set_to_map();
+
+    try
+    {
+        for (const auto& [button_value, path] : parser.get_single_buttons())
+        {
+            std::wcout << button_value << " " << path << '\n';
+        }
+    }
+    catch (const std::exception& ex)
+    {
+        std::cout << ex.what() << '\n';
+    }
 
     return 0;
 }
