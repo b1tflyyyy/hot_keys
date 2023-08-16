d32e817ac2a77c12a8a1b51b77880d2339254e07
b1tfly <thisisandrew0110@gmail.com>
2023-08-16 15:15:48 +0300

    removed setters

--- hot_keys/button/button.cpp
+++ hot_keys/button/button.cpp
@@ -1,61 +1,73 @@
 #include "button.hpp"
 
+Button::Button(const bool is_double_button, 
+               const std::int32_t first_button, 
+               const std::int32_t second_button, 
+               const std::wstring& app_path,
+               const std::wstring& app_name) :
+    m_is_double_button(is_double_button), 
+    m_first_button(first_button), 
+    m_second_button(second_button),
+    m_app_path(app_path),
+    m_app_name(app_name)
+{ }
+
 Button::Button(Button&& other) noexcept
 {
     this->m_first_button = other.m_first_button;
     this->m_second_button = other.m_second_button;
     this->m_app_path = std::move(other.m_app_path);
     this->m_app_name = std::move(other.m_app_name);
-    this->m_is_single_button = other.m_is_single_button;
+    this->m_is_double_button = other.m_is_double_button;
 }
 
-const int Button::get_first_button() const
+const std::int32_t Button::get_first_button() const
 {
     return m_first_button;
 }
 
-const int Button::get_second_button() const
+const std::int32_t Button::get_second_button() const
 {
     return m_second_button;
 }
 
-void Button::set_button(const int first_value, const int second_value)
-{
-    m_first_button = first_value;
-    m_second_button = second_value;
-}
+//void Button::set_button(const std::int32_t first_value, const std::int32_t second_value)
+//{
+//    m_first_button = first_value;
+//    m_second_button = second_value;
+//}
 
 const std::wstring& Button::get_app_path() const
 {
     return m_app_path;
 }
 
-void Button::set_app_path(const std::wstring& app_path)
-{
-    m_app_path = app_path;
-}
+//void Button::set_app_path(const std::wstring& app_path)
+//{
+//    m_app_path = app_path;
+//}
 
 const std::wstring& Button::get_app_name() const
 {
     return m_app_name;
 }
 
-void Button::set_app_name(const std::wstring& app_name)
+//void Button::set_app_name(const std::wstring& app_name)
+//{
+//    m_app_name = app_name;
+//}
+
+const bool Button::is_double_button() const
 {
-    m_app_name = app_name;
+    return m_is_double_button;
 }
 
-const bool Button::is_single_button() const
-{
-    return m_is_single_button;
-}
+//void Button::set_is_single_button(const bool is_single_button)
+//{
+//    m_is_double_button = is_single_button;
+//}
 
-void Button::set_is_single_button(const bool is_single_button)
-{
-    m_is_single_button = is_single_button;
-}
-
-int Button::get_closer_button()
+std::int32_t Button::get_closer_button()
 {
     return m_closer_button;
 }
--- hot_keys/button/button.hpp
+++ hot_keys/button/button.hpp
@@ -3,40 +3,46 @@
 #define BUTTON_HPP
 
 #include <string>
+#include <cstdint>
 
 class Button final
 {
 public:
     Button() = default;
-    
-    inline ~Button() noexcept = default;
+    explicit Button(const bool is_double_button,
+                    const std::int32_t first_button,
+                    const std::int32_t second_button,
+                    const std::wstring& app_path,
+                    const std::wstring& app_name);
 
-    constexpr Button(const Button& other) = default;
+    ~Button() = default;
+
+    Button(const Button& other) = default;
     Button(Button&& other) noexcept;
 
-    void set_button(const int first_value, const int second_value);
+    // void set_button(const std::int32_t first_value, const std::int32_t second_value = 0);
 
-    const int get_first_button() const;    
-    const int get_second_button() const;
+    const std::int32_t get_first_button() const;
+    const std::int32_t get_second_button() const;
 
     const std::wstring& get_app_path() const;
-    void set_app_path(const std::wstring& app_path);
+    // void set_app_path(const std::wstring& app_path);
 
     const std::wstring& get_app_name() const;
-    void set_app_name(const std::wstring& app_name);
+    // void set_app_name(const std::wstring& app_name);
 
-    const bool is_single_button() const;
-    void set_is_single_button(const bool is_single_button);
+    const bool is_double_button() const;
+    // void set_is_single_button(const bool is_single_button);
 
-    static int get_closer_button();
+    static std::int32_t get_closer_button();
 
 private:
-    int m_first_button;
-    int m_second_button;
+    std::int32_t m_first_button;
+    std::int32_t m_second_button;
     std::wstring m_app_path;
     std::wstring m_app_name;
-    bool m_is_single_button;
-    constexpr static int m_closer_button = 0x2E; // delete button
+    bool m_is_double_button;
+    constexpr static std::int32_t m_closer_button = 0x2E; // delete button
 };
 
 #endif // !BUTTON_HPP
