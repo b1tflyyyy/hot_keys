86ea912b354d1665760f9f88efa6c1c2915cef26
b1tfly <thisisandrew0110@gmail.com>
2023-08-16 15:21:00 +0300

    added first version of token parsing system

--- hot_keys/parser/parser.cpp
+++ hot_keys/parser/parser.cpp
@@ -4,145 +4,177 @@ Parser::Parser() :
     m_path(std::move(L"config.txt"))
 { }
 
-void Parser::parse_all_data()
-{
-    m_wfin.open(m_path, std::ios::in);
-    m_wfin.imbue(std::locale(""));
-
-    if (!m_wfin.is_open())
-    {
-        throw std::exception("ERROR WITH FILE, FILE WAS NOT FOUND!!!");
-    }
-
-    m_all_data.reserve(10);
-
-    std::wstring temp_data{};
-    while (std::getline(m_wfin, temp_data))
-    {
-        m_all_data.push_back(std::move(temp_data));
-    }
-    
-    m_all_data.shrink_to_fit();
-
-#if DEBUG_LOGS
-    std::wcout << L"parse_all_data() --->\n";
-    for (const auto& el : m_all_data)
-    {
-        std::wcout << el << '\n';
-    }
-    std::wcout << L"--------------------\n\n";
-#endif // !DEBUG_LOGS
-}
-
 const std::vector<Button>& Parser::get_buttons() const
 {
     return m_buttons;
 }
 
-bool Parser::m_is_single_button(const std::wstring_view wstr)
+std::unique_ptr<IError> Parser::separate_input_data()
 {
-    auto it = std::find(wstr.begin(), wstr.end(), ':');
-    wchar_t value = *(it + 2);
+    std::locale::global(std::locale(""));
 
-    if (value != L'0' && value != L'1')
+    m_wfin.open(m_path, std::ios::in);
+    m_wfin.imbue(std::locale(""));
+
+    if (!m_wfin.is_open())
     {
-        throw std::exception("ERROR: param double_buttons can be only 0 or 1!");
+        return std::make_unique<file_error_info>(file_error_info(m_path, L"ERROR opening the file "));
     }
 
-    return value == L'0';
-}
+    m_separated_data.reserve(10);
 
-std::pair<int, int> Parser::m_parse_button_value(const std::wstring& wstr)
-{
-    if (!m_is_single_button(wstr))
+    std::wstring temp_data{};
+    while (std::getline(m_wfin, temp_data, L';'))
     {
-        // TODO: redo this
-        std::wstring first_button_value{};
-        first_button_value.reserve(4);
-
-        std::wstring second_button_value{};
-        second_button_value.reserve(4);
-
-        auto it_first_button = std::find(wstr.begin(), wstr.end(), L'1');
-        for (it_first_button += 14; *it_first_button != L';'; ++it_first_button)
-        {
-            first_button_value += *it_first_button;
-        }
-
-        auto it_second_button = std::find(wstr.begin(), wstr.end(), L'2');
-        for (it_second_button += 4; *it_second_button != L';'; ++it_second_button)
-        {
-            second_button_value += *it_second_button;
-        }
-
-#if DEBUG_LOGS
-        std::wcout << "first value: " << first_button_value << " " << "second value: " << second_button_value << '\n';
-#endif // DEBUG_LOGS
-
-        return std::make_pair(std::stoi(first_button_value, nullptr, 16), std::stoi(second_button_value, nullptr, 16));
+        remove_bad_symbols(temp_data); // remove bad symbols also remove ' ' in path, think about it
+        m_separated_data.push_back(std::move(temp_data));
     }
-
-    auto it = std::find(wstr.begin(), wstr.end(), L'=');
     
-    std::wstring tmp{};
-    tmp.reserve(4);
+    m_wfin.close();
+    return std::make_unique<file_error_info>(file_error_info());
+}
 
-    for (it += 2; *it != L';'; ++it)
+__inline void Parser::remove_bad_symbols(std::wstring& data)
+{
+    data.erase(std::remove_if(data.begin(), data.end(), [](char symbol) -> bool 
     {
-        tmp += *it;
+        return symbol == L' ' || symbol == L'\n';
+    }), data.end());
+}
+
+std::unique_ptr<IError> Parser::parse_token_entry(const std::wstring& data, const token_index token_index)
+{
+    std::wstring expected_data = token_storage::token[token_index];
+
+    std::size_t i{};
+    for (; i < expected_data.size(); ++i)
+    {
+        if (data[i] != expected_data[i])
+        {
+            // error with line number, here it is 0
+            return std::make_unique<parsing_error_info>(parsing_error_info(expected_data, data, i, data[i], 0, L"parsing error!"));
+        }
     }
 
-#if DEBUG_LOGS
-    std::wcout << L"m_parse_button_value(): " << tmp << '\n';
-#endif // !DEBUG_LOGS
-
-    return std::make_pair(std::stoi(tmp, nullptr, 16), 0);
+    m_token_entries.push_back(token_entry({ expected_data, std::wstring(data.begin() + i, data.end()) }));
+    return std::make_unique<parsing_error_info>(parsing_error_info());
 }
 
-void Parser::m_parse_path(std::wstring& wstr)
+void Parser::create_buttons()
 {
-    wstr.erase(wstr.begin(), ++std::find(wstr.begin(), wstr.end(), L'>'));
-    wstr.erase(wstr.find_last_of(L';'));
-
-#if DEBUG_LOGS
-    std::wcout << L"m_parse_path(): " << wstr << '\n';
-#endif // !DEBUG_LOGS
-}
-
-void Parser::m_parse_app_name(std::wstring& wstr)
-{
-    wstr.erase(NULL, wstr.find_last_of(L"\\") + 1);
-
-#if DEBUG_LOGS
-    std::wcout << L"m_parse_app_name(): " << wstr << '\n';
-#endif
-}
-
-void Parser::load_all_data_to_ram()
-{
-    std::pair<int, int> tmp_values{};
-    std::wstring tmp_info{};
-    bool is_single_button{};
-
-    m_buttons.reserve(10);
-
-    for (int i = 0; i < m_all_data.size(); ++i)
+    for (std::size_t i{}; i < m_token_entries.size();)
     {
-        Button button{};
-        tmp_info = m_all_data[i];
+        if (m_token_entries[i].get_value(token_storage::token[token_index::is_double_button]) == L"1")
+        {
+            create_double_button(i);
+            continue;
+        }
 
-        is_single_button = m_is_single_button(tmp_info);
-        button.set_is_single_button(is_single_button);
+        create_single_button(i);
+    }
+}
 
-        tmp_values = m_parse_button_value(tmp_info);
-        button.set_button(tmp_values.first, tmp_values.second);
+void Parser::parse_token_entries()
+{
+    for (std::size_t i{}; i < m_separated_data.size();)
+    {
+        if (is_double_button(m_separated_data[i]))
+        {
+            parse_double_button(i);
+            continue;
+        }
 
-        m_parse_path(tmp_info);
-        button.set_app_path(tmp_info);
+        parse_single_button(i);
+    }
+}
 
-        m_parse_app_name(tmp_info);
-        button.set_app_name(tmp_info);
+const bool Parser::is_double_button(const std::wstring& data)
+{
+    std::wstring double_button_value(data.begin() + token_storage::token[token_index::is_double_button].size(), data.end());
+    return std::stoi(double_button_value);
+}
 
-        m_buttons.push_back(std::move(button));
+std::unique_ptr<IError> Parser::parse_single_button(std::size_t& current_index)
+{
+    auto error = parse_token_entry(m_separated_data[current_index++], token_index::is_double_button);
+    error = parse_token_entry(m_separated_data[current_index++], token_index::first_button);
+    error = parse_token_entry(m_separated_data[current_index++], token_index::path);
+
+    return error;
+}
+
+std::unique_ptr<IError> Parser::parse_double_button(std::size_t& current_index)
+{
+    auto error = parse_token_entry(m_separated_data[current_index++], token_index::is_double_button);
+    error = parse_token_entry(m_separated_data[current_index++], token_index::first_button);
+    error = parse_token_entry(m_separated_data[current_index++], token_index::second_button);
+    error = parse_token_entry(m_separated_data[current_index++], token_index::path);
+
+    return error;
+}
+
+void Parser::create_single_button(std::size_t& current_index)
+{
+    bool is_double_button_value = std::stoi(m_token_entries[current_index++].get_value(token_storage::token[token_index::is_double_button]));
+    std::int32_t first_button_value = std::stoi(m_token_entries[current_index++].get_value(token_storage::token[token_index::first_button]), nullptr, 16);
+    std::wstring app_path_value = m_token_entries[current_index++].get_value(token_storage::token[token_index::path]);
+
+    // app_name is blank here
+    m_buttons.emplace_back(is_double_button_value, first_button_value, 0, app_path_value, L"");
+}
+
+void Parser::create_double_button(std::size_t& current_index)
+{
+    bool is_double_button_value = std::stoi(m_token_entries[current_index++].get_value(token_storage::token[token_index::is_double_button]));
+    std::int32_t first_button_value = std::stoi(m_token_entries[current_index++].get_value(token_storage::token[token_index::first_button]), nullptr, 16);
+    std::int32_t second_button_value = std::stoi(m_token_entries[current_index++].get_value(token_storage::token[token_index::second_button]), nullptr, 16);
+    std::wstring app_path_value = m_token_entries[current_index++].get_value(token_storage::token[token_index::path]);
+
+    // app_name is blank here
+    m_buttons.emplace_back(is_double_button_value, first_button_value, second_button_value, app_path_value, L"");
+}
+
+/* ======================================== TEST PART ======================================== */
+void Parser::show_parsed()
+{
+    std::wcout << "======================================== parsed input data ========================================\n";
+    for (std::size_t i{}; i < m_separated_data.size(); ++i)
+    {
+        std::wcout << m_separated_data[i];
+        std::wcout << std::endl;
+    }
+    std::wcout << std::endl;
+    std::wcout << std::endl;
+}
+
+void Parser::show_tokens()
+{
+    std::wcout << "======================================== token entries ========================================\n";
+
+    for (std::size_t i{}; i < m_token_entries.size();)
+    {
+        if (m_token_entries[i].get_value(token_storage::token[token_index::is_double_button]) == L"1")
+        {
+            std::wcout << m_token_entries[i++].get_value(token_storage::token[token_index::is_double_button]) << '\n';
+            std::wcout << m_token_entries[i++].get_value(token_storage::token[token_index::first_button]) << '\n';
+            std::wcout << m_token_entries[i++].get_value(token_storage::token[token_index::second_button]) << '\n';
+            std::wcout << m_token_entries[i++].get_value(token_storage::token[token_index::path]) << '\n';
+
+            continue;
+        }
+
+        std::wcout << m_token_entries[i++].get_value(token_storage::token[token_index::is_double_button]) << '\n';
+        std::wcout << m_token_entries[i++].get_value(token_storage::token[token_index::first_button]) << '\n';
+        std::wcout << m_token_entries[i++].get_value(token_storage::token[token_index::path]) << '\n';
+    }
+}
+
+void Parser::show_buttons()
+{
+    std::wcout << "======================================== BUTTONS ========================================\n";
+
+    for (const auto& el : m_buttons)
+    {
+        std::wcout << L"first button: " << el.get_first_button() << " second button: " << el.get_second_button() << L" app_path: " << el.get_app_path() << L" app_name: " << el.get_app_name() << L" closer_button: " << el.get_closer_button() << '\n';
     }
 }
--- hot_keys/parser/parser.hpp
+++ hot_keys/parser/parser.hpp
@@ -2,16 +2,22 @@
 #ifndef PARSER_HPP
 #define PARSER_HPP
 
-#define DEBUG_LOGS false
-
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <fstream>
 #include <vector>
 #include <locale>
 #include <algorithm>
+#include <memory>
 
 #include "../button/button.hpp"
+
+#include "token_entry/token_entry.hpp"
+#include "token_storage/token_storage.hpp"
+
+#include "parser_errors/file_error/file_error.hpp"
+#include "parser_errors/parsing_error/parsing_error.hpp"
 
 class Parser final
 {
@@ -22,23 +28,36 @@ public:
     Parser(const Parser& other) = delete;
     Parser(const Parser&& other) = delete;
 
-    void parse_all_data();
-    void load_all_data_to_ram();
-
     const std::vector<Button>& get_buttons() const;
+    
+    std::unique_ptr<IError> separate_input_data(); // separate data from txt file by ';'
+    __inline void remove_bad_symbols(std::wstring& data); // remove '\n' && ' '
+
+    std::unique_ptr<IError> parse_token_entry(const std::wstring& data, const token_index token_index);
+    void create_buttons();
+
+    void parse_token_entries();
+    const bool is_double_button(const std::wstring& data);
+
+    std::unique_ptr<IError> parse_single_button(std::size_t& current_index);
+    std::unique_ptr<IError> parse_double_button(std::size_t& current_index);
+
+    void create_single_button(std::size_t& current_index);
+    void create_double_button(std::size_t& current_index);
+
+    // TEST funcs
+    void show_parsed();
+    void show_tokens();
+    void show_buttons();
 
 private:
-    std::wstring m_path;
+    const std::wstring m_path;
     std::wifstream m_wfin;
 
-private:
     std::vector<Button> m_buttons;
-    std::vector<std::wstring> m_all_data;
     
-    std::pair<int, int> m_parse_button_value(const std::wstring& wstr);
-    void m_parse_app_name(std::wstring& wstr);
-    void m_parse_path(std::wstring& wstr);
-    bool m_is_single_button(const std::wstring_view wstr);
+    std::vector<std::wstring> m_separated_data;
+    std::vector<token_entry> m_token_entries;
 };
 
 #endif // !PARSER_HPP
